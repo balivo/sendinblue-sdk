268c09c27371cf014fa403a2a08f63a48bd08291
Martinus Suherman <martinus.suherman@gmail.com>
2020-06-19 13:24:20 +0700

    WIP - Rearrange and add properties to match SendInBlue API reference

--- src/Balivo.SendInBlue/Balivo.SendInBlue/SmtpSendTransactionalArgs.cs
+++ src/Balivo.SendInBlue/Balivo.SendInBlue/SmtpSendTransactionalArgs.cs
@@ -5,25 +5,43 @@ namespace Balivo.SendInBlue
 {
     public sealed class SmtpSendTransactionalArgs
     {
+        [JsonProperty("sender")]
+        public SendInBlueEmailInfo Sender { get; set; }
+
+        [JsonProperty("to")]
+        public List<SendInBlueEmailInfo> To { get; set; }
+
         [JsonProperty("bcc")]
         public List<SendInBlueEmailInfo> Bcc { get; set; }
+
+        [JsonProperty("cc")]
+        public List<SendInBlueEmailInfo> Cc { get; set; }
 
         [JsonProperty("htmlContent")]
         public string HtmlContent { get; set; }
 
-        [JsonProperty("replyTo")]
-        public SendInBlueEmailInfo ReplyTo { get; set; }
-
-        [JsonProperty("sender")]
-        public SendInBlueEmailInfo Sender { get; set; }
+        [JsonProperty("textContent")]
+        public string TextContent { get; set; }
 
         [JsonProperty("subject")]
         public string Subject { get; set; }
 
-        [JsonProperty("textContent")]
-        public string TextContent { get; set; }
+        [JsonProperty("replyTo")]
+        public SendInBlueEmailInfo ReplyTo { get; set; }
 
-        [JsonProperty("to")]
-        public List<SendInBlueEmailInfo> To { get; set; }
+        // [JsonProperty("attachment")]
+        // public List<string> Attachment { get; set; }
+
+        // [JsonProperty("headers")]
+        // public List<string> Headers { get; set; }
+
+        [JsonProperty("templateId")]
+        public long TemplateId { get; set; }
+
+        // [JsonProperty("params")]
+        // public List<string> Params { get; set; }
+
+        [JsonProperty("tags")]
+        public List<string> Tags { get; set; }
     }
 }
